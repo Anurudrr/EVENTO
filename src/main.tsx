0f32002c0079b4bffdaacdd3c252ab6b57c02246
anurudrr <sanurudh938@gmail.com>
2026-08-24 13:22:25 +0000

    Fix: Add global Leaflet CSS import to fix map rendering

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AppErrorBoundary } from './components/AppErrorBoundary.tsx';
+import 'leaflet/dist/leaflet.css';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
